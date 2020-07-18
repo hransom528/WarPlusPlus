d4cf20804a72108384a618a9f2a4ece5d7405042
Harris Ransom <hransom528@gmail.com>
2020-07-17 22:58:02 -0400

    Updated sources to try and fix memory leaks

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include <sstream>
 #include <vector>
 
-#include "Card.h"
-#include "Deck.h"
+#include "include/Card.h"
+#include "include/Deck.h"
 
 using namespace std;
 
@@ -13,6 +13,7 @@ static Deck deck;
 static vector<Card> player1;
 static vector<Card> player2;
 static int warCard = 4;
+static char loopChar = 'y';
 void reset();
 
 /** Functions **/
@@ -31,7 +32,6 @@ static void goToWar() {
                 player1.push_back(player2.front());
                 player2.erase(player2.begin());
             }
-
         }
     }
     else if (player1.at(warCard - 1).compareTo(player2.at(warCard - 1)) < 0) {
@@ -56,13 +56,14 @@ static void goToWar() {
 void reset() {
     deck.reset();
     warCard = 4;
+    loopChar = 'y';
+    fflush(stdin);
     player1.clear();
     player2.clear();
 }
 
 /** Main **/
 int main() {
-    char loopChar = 'y';
     while (loopChar == 'y' && loopChar != 'n') {
         deck.shuffle();
 
@@ -73,26 +74,26 @@ int main() {
         }
 
         //While both players still have cards
-        while ((!player1.empty()) && (!player2.empty())) {
+        while (!player1.empty() && !player2.empty()) {
             cout << "Player 1 plays: " << player1.front().toString() << endl;
             cout << "Player 2 plays: " << player2.front().toString() << endl;
 
             //Compares cards drawn
-            if (player1.at(0).compareTo(player2.at(0)) > 0) {
+            if (player1.front().compareTo(player2.front()) > 0) {
                 cout << "Player 1 wins" << endl;
-                player1.push_back(player1.at(0));
+                player1.push_back(player1.front());
                 player1.erase(player1.begin());
                 if (player2.size() > 0) {
-                    player1.push_back(player2.at(0));
+                    player1.push_back(player2.front());
                     player2.erase(player2.begin());
                 }
             }
-            else if (player1.at(0).compareTo(player2.at(0)) < 0) {
+            else if (player1.front().compareTo(player2.front()) < 0) {
                 cout << "Player 2 wins" << endl;
-                player2.push_back(player2.at(0));
+                player2.push_back(player2.front());
                 player2.erase(player2.begin());
                 if (player1.size() > 0) {
-                    player2.push_back(player1.at(0));
+                    player2.push_back(player1.front());
                     player1.erase(player1.begin());
                 }
             }
@@ -116,13 +117,10 @@ int main() {
         // Gets input for next round
         cout << "Would you like to simulate another game (y/n)?" << endl;
         cin >> loopChar;
-        while (loopChar != 'y' && loopChar != 'n') {
-            cout << "Please input a valid response: " << endl;
-            cin >> loopChar;
+        if ((loopChar == 'y') || (loopChar == 'Y')) {
+            // Resets for next round
+            reset();
         }
-
-        // Resets for next round
-        reset();
     }
 
     // Ends game
